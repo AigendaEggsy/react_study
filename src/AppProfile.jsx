5064a9b3fcc1c0ecaf5213ac90dc2876f7876cbf
AigendaEggsy <eggsy@aigenda.me>
2024-03-09 16:47:13 +0900

    4.14 Event

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -5,8 +5,17 @@ import Profile from './components/Profile';
 function AppProfile() {
   // const name = "EGGSY"
   // const list = ['우유', '딸기', '바나나', '요거트']
+  const handleClick = (event) => {
+    console.log(event);
+    alert("버튼이 클릭됨")
+  }
   return (
     <>
+      <button onClick={handleClick}>버튼</button>
+      <Avartar
+        image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
+        isNew={true}
+      />
       <Profile
         image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
         name='James Kim'
