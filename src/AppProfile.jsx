66ede97f01d12d20a7b8a800e6151877db990eeb
AigendaEggsy <eggsy@aigenda.me>
2024-03-09 14:49:32 +0900

    4.9 use Props

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -2,13 +2,25 @@ import './App.css';
 import Profile from './components/Profile';
 
 function AppProfile() {
-  const name = "EGGSY"
-  const list = ['우유', '딸기', '바나나', '요거트']
+  // const name = "EGGSY"
+  // const list = ['우유', '딸기', '바나나', '요거트']
   return (
     <>
-      <Profile/>
-      <Profile/>
-      <Profile/>
+      <Profile
+        image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
+        name='James Kim'
+        title='frontend'
+      />
+      <Profile
+        image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
+        name='Anna Young'
+        title='backend'
+      />
+      <Profile
+        image='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80'
+        name='Bob Yu'
+        title='python'
+      />
     </>
   );
 }
